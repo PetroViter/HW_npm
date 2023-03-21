0bb1bd9b85c4ed2e16a18bfa447de34536abd7d7
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-21 13:26:46 +0200

    +

---
+++ index.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+  return a + b;
+}
+sum();
+
+function sub(a, b) {
+  return a - b;
+}
+sub();
+
+function multiplication(a, b) {
+  return a * b;
+}
+multiplication();
+
+function division(a, b) {
+  return a + b;
+}
+division();
+
+
